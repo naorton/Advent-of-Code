4a0ef3bd2e84de3613125d842e4426f345708a35
nathen <naorton@gmail.com>
2018-12-30 16:16:59 -0600

    Advent of code day 12 part 1

---
+++ 2015/Day 12/day12.py
@@ -0,0 +1,38 @@
+
+import json
+from pprint import pprint
+
+with open('data.json') as f:
+	data = json.load(f)
+
+def arrayChecker(array, count):
+	for entry in array:
+		if type(entry) == int:
+			count += entry
+		elif type(entry) == list:
+			count = arrayChecker(entry, count)
+		elif type(entry) == dict:
+			count = typeChecker(entry, count)
+	return count
+
+def typeChecker(data, count):
+	if type(data) == dict:
+		for entry in data:
+			if type(entry) == str:
+				temp = data[entry]
+				count = typeChecker(temp, count)
+			elif type(entry) == int:
+				count += entry
+			elif type(entry) == list:
+				count = arrayChecker(entry, count)
+
+	elif type(data) == int:
+		count += data
+	elif type(data) == list:
+		count = arrayChecker(data, count)
+	return count 
+
+
+count = 0
+answer = typeChecker(data, count)
+print(answer)
